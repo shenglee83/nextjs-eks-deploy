796d6d8a0142b2db41020838152b51ff84abac82
Gan Sheng Lee <maxagns@MacBook-Pro-Administrator.local>
2023-05-18 11:53:05 +0800

    Get environment variables via file from S3 bucket

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log(process.env.API_VARIABLE);
   res.status(200).json({ name: 'John Doe', value: process.env.API_VARIABLE })
 }
