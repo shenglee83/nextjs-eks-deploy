46c8b8c860cca2289d4de998bbdd92eff0f0516a
Sheng Lee Gan <1111113168@student.mmu.edu.my>
2023-07-13 12:07:55 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,5 +3,6 @@
 export default function handler(req, res) {
   console.log(`Policy Wording: ${process.env.ESKIN_MOTOR_POLICY_WORDING}`);
   console.log(`PDS: ${process.env.ESKIN_MOTOR_PDS}`);
+  console.log(`PDS_1: ${process.env.ESKIN_MOTOR_PDS_1}`);
   res.status(200).json({ name: 'John Doe', value: process.env.API_VARIABLE })
 }
