73693d270afa734672e7241adca3d843eb5173a8
Sheng Lee Gan <1111113168@student.mmu.edu.my>
2023-07-13 11:08:02 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log(process.env);
+  console.log(`Policy Wording: ${process.env.ESKIN_MOTOR_POLICY_WORDING}`);
+  console.log(`PDS: ${process.env.ESKIN_MOTOR_PDS}`);
   res.status(200).json({ name: 'John Doe', value: process.env.API_VARIABLE })
 }
