e68276269d2d2bd12406a93731abd5d80a9dcc03
Sheng Lee Gan <1111113168@student.mmu.edu.my>
2023-07-13 15:21:57 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -6,5 +6,6 @@ export default function handler(req, res) {
   console.log(`PDS_1: ${process.env.ESKIN_MOTOR_PDS_1}`);
   console.log(`MERCHANT_CODE: ${process.env.MERCHANT_CODE}`);
   console.log(`IPAY88_URL: ${process.env.IPAY88_URL}`);
+  console.log(`MY_GI_APP_SECRET: ${process.env.MY_GI_APP_SECRET}`);
   res.status(200).json({ name: 'John Doe', value: process.env.API_VARIABLE })
 }
